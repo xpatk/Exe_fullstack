b0a7712ab4fb6475fafeeef95a05dd0d904c3280
xpat <patrycja.kruczynska@gmail.com>
2024-08-05 16:51:42 +0200

    servers js adapted

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log("Requete recue");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
   res.json({ message: "Votre requete a bien ete recue" });
+  next();
+});
+
+app.use((req, res) => {
+  console.log("Reponse sent with success");
 });
 
 module.exports = app;
