f2503814fbc178dee44f6f813acbfb2288b6c4d3
xpat <patrycja.kruczynska@gmail.com>
2024-08-07 12:44:48 +0200

    route GET for all objects

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+const Thing = require("./models/Thing");
 
 app.use(express.json());
 
@@ -28,32 +29,20 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ message: "Object created!" });
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Object registered" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
+app.use("/api/stuff", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
