1c751b717c01e7fed9aae4226abaa2f50c8def42
xpat <patrycja.kruczynska@gmail.com>
2024-08-07 11:20:40 +0200

    created schema for objects for sale

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://user-random:JaSBILhV34yrBZq2@cluster0.snlkboy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
