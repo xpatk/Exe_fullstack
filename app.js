39ef21c03956a857cb7a25a58e7b21f8ae578857
xpat <patrycja.kruczynska@gmail.com>
2024-08-07 13:40:08 +0200

    added route for delete

--- app.js
+++ app.js
@@ -39,7 +39,27 @@ app.post("/api/stuff", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
-app.use("/api/stuff", (req, res, next) => {
+app.put("/api/stuff/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() =>
+      res.status(200).json({ message: "Object modified successfully." })
+    )
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Object deleted" }))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/stuff/:id", (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/stuff", (req, res, next) => {
   Thing.find()
     .then((things) => res.status(200).json(things))
     .catch((error) => res.status(400).json({ error }));
