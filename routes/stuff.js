ccb5f3338c21cefcbe55b988e2e07731ef9196a9
xpat <patrycja.kruczynska@gmail.com>
2024-08-12 12:13:32 +0200

    middleware auth

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const auth = require("auth");
 const router = express.Router();
+
 const stuffCtrl = require("../controllers/stuff");
 
-router.post("/", stuffCtrl.createThing);
-router.put("/:id", stuffCtrl.modifyThing);
-router.delete("/:id", stuffCtrl.deleteThing);
-router.get("/:id", stuffCtrl.getOneThing);
-router.get("/", stuffCtrl.getAllThings);
+router.get("/", auth, stuffCtrl.getAllThings);
+router.post("/", auth, stuffCtrl.createThing);
+router.put("/:id", auth, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+router.get("/:id", auth, stuffCtrl.getOneThing);
 
 module.exports = router;
